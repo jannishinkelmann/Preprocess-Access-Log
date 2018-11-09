903cc46636a1340ffe04b7c38c4110e557ecb998
Jannis Hinkelmann <jannis.hinkelmann@icloud.com>
2018-11-09 17:12:05 +0100

    Refactoring, removed verbose
    underscore-naming convention for private functions
    call function only if main
    no global variable for argsa

--- processLog.py
+++ processLog.py
@@ -1,80 +1,56 @@
 #!/usr/bin/python
-# Custom Log parser
 import sys
 import os
 import gzip
 import argparse
 
-def main():
-    parseArguments()
+def processLog(outputFile, inputFile, domainName=[], anonymizeIp=False, zipfile=False):
+    if os.path.isfile(outputFile):
+        raise OSError('File ' + outputFile + ' already exists.')
 
-    if os.path.isfile(args.outputFile):
-        vPrint('Existence check of output file was positive. Break.')
-        raise OSError('File ' + args.outputFile + ' already exists.')
-
-    if args.zipfile:
-        vPrint('Opening input (gzip) and output files.')
-        with gzip.open(args.inputFile,'r') as srcFile, open(args.outputFile, 'w') as dstFile:
-            processFile(srcFile, dstFile)
+    if zipfile:
+        with gzip.open(inputFile,'r') as srcFile, open(outputFile, 'w') as dstFile:
+            _processFile(srcFile, dstFile, domainName, anonymizeIp)
     else:
-        vPrint('Opening input and output files.')
-        with open(args.inputFile,'r') as srcFile, open(args.outputFile, 'w') as dstFile:
-            processFile(srcFile, dstFile)
+        with open(inputFile,'r') as srcFile, open(outputFile, 'w') as dstFile:
+            _processFile(srcFile, dstFile, domainName, anonymizeIp)
 
-def vPrint(msg):
-    if args.verbose: print(msg)
-
-def anonymizeIp(str):
+def _anonymizeIp(str):
     return '- ' + str.split(' ', 1)[1]
 
-def optAnonymizeIp(str):
-    if args.anonymizeIp:
-        vPrint('..Replace http client\'s IP address.')
-        str = anonymizeIp(str)
-    return str
-
-def removeDomainName(str):
-    vPrint('..Remove domain name.')
+def _removeDomainName(str):
     split = str.split(' ', 2)
     return split[0] +' '+ split[2]
 
-def getDomainName(str):
+def _getDomainName(str):
     return str.split(' ')[1]
 
-def isWatchAllSet():
-    return len(args.domainName) == 0
+def _isWatchAllSet(domainName):
+    return len(domainName) == 0
 
-def isDomainToBeWatched(domain):
-    return domain in args.domainName
+def _isDomainToBeWatched(domain, domainName):
+    return domain in domainName
 
-def processLine(line, dstFile):
-    if isDomainToBeWatched(getDomainName(line)) or isWatchAllSet():
-        str = removeDomainName(line)
-        str = optAnonymizeIp(str)
-        vPrint('..Write Line to output file.')
+def _processLine(line, dstFile, domainName, anonymizeIp):
+    if _isDomainToBeWatched(_getDomainName(line), domainName) or _isWatchAllSet(domainName):
+        str = _removeDomainName(line)
+        if anonymizeIp: str = _anonymizeIp(str)
         dstFile.write(str)
 
-def processFile(srcFile, dstFile):
-    vPrint('Begin reading input file.')
+def _processFile(srcFile, dstFile, domainName, anonymizeIp):
     line = srcFile.readline()
     lineNum = 1
     while line:
-        vPrint('Read line %04d' % (lineNum,))
-        processLine(line, dstFile)
+        _processLine(line, dstFile, domainName, anonymizeIp)
         line = srcFile.readline()
         lineNum += 1
 
-def parseArguments():
-    global args
+if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument('inputFile', help='log file to read and parse')
     argparser.add_argument('outputFile', help='output file, write target')
     argparser.add_argument('domainName', help='domain(s) for which entries are filtered', nargs='*')
     argparser.add_argument('-a', '--anonymizeIp', help='skip client\'s IP address', action='store_true')
     argparser.add_argument('-z', '--zipfile', help='open Gzip input file', action='store_true')
-    argparser.add_argument('-v', '--verbose', help='run in verbose mode', action='store_true')
     args = argparser.parse_args()
-
-args = 'global arguments'
-
-main()
+    processLog(args.outputFile, args.inputFile, args.domainName, args.anonymizeIp, args.zipfile)
