4d4b9f0814e270e00c264a71097cab79af74e5c0
Jannis Hinkelmann <jannis.hinkelmann@icloud.com>
2018-11-17 11:11:48 +0100

    fix parselog

--- importLog.py
+++ importLog.py
@@ -1,43 +1,50 @@
 import os
 import argparse
-import configparser
 import processlog
+import ConfigParser
 
 
-config = configparser.configparser()
-if os.path.isfile('settings.ini'):
-    config.read()
-else:
-    raise OSError('settings.ini not found')
+def getconfig():
+    config = ConfigParser.ConfigParser()
 
-
-PROCESS_SCRIPT_PATH = config.get('importer', 'process_script_path')
-IMPORT_SCRIPT_PATH = config.get('importer', 'import_script_path')
-DOMAINS = config.get('importer', 'domains')
-ANONYMIZE_IP = config.get('importer', 'anonymize_ip')
-TMP_FILE_NAME = config.get('importer', 'temp_file_name')
-MATOMO_URL = config.get('matomo', 'url')
-MATOMO_USER = config.get('matomo', 'user')
-MATOMO_PASS = config.get('matomo', 'pass')
-MATOMO_SITE_ID = config.get('matomo', 'site_id')
-MATOMO_OPTIONS = config.get('matomo', 'options')
-
-
-if os.path.isfile():
-    raise OSError('Cannot write temp file. File ' + TMP_FILE_NAME + ' already exists.')
-
-
-def importLog(input):
-    if os.path.isdir(input):
-        for file in os.listdir(input):
-            processlog.processLog(file, TMP_FILE_NAME, DOMAINS, ANONYMIZE_IP)
+    if os.path.isfile('settings.ini'):
+        config.read('settings.ini')
     else:
-        if os.path.isfile(input):
-            processlog.processLog(input, TMP_FILE_NAME, DOMAINS, ANONYMIZE_IP)
+        raise OSError('settings.ini not found')
+
+    return config
+
+    # getconfig().get('importer', 'process_script_path')
+    # getconfig().get('importer', 'import_script_path')
+    # str.split(',', getconfig().get('importer', 'domains'))
+    # getconfig().getboolean('importer', 'anonymize_ip')
+    # getconfig().get('importer', 'temp_file_name')
+    # getconfig().get('matomo', 'url')
+    # getconfig().get('matomo', 'user')
+    # getconfig().get('matomo', 'pass')
+    # getconfig().get('matomo', 'site_id')
+    # getconfig().get('matomo', 'options')
+
+
+def importlogdir(inputPath):
+    if os.path.isfile(getconfig().get('importer', 'temp_file_name')):
+        raise OSError('Cannot write temp file. File ' + getconfig().get('importer', 'temp_file_name') + ' already exists.')
+
+    if os.path.isdir(inputPath):
+        for file in os.listdir(inputPath):
+            _importlog(inputPath)
+    else:
+        _importlog(inputPath)
+
+
+def _importlog(inputPath):
+    if os.path.isfile(inputPath):
+        processlog.processLog(inputPath, getconfig().get('importer', 'temp_file_name'), str.split(',', getconfig().get('importer', 'domains')), getconfig().getboolean('importer', 'anonymize_ip'))
+        os.remove(getconfig().get('importer', 'temp_file_name'))
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('input', help='log file or dir of log files to import')
+    argparser.add_argument('inputPath', help='log file or dir of log files to import')
     args = argparser.parse_args()
-    importLog(args.input)
+    importlogdir(args.inputPath)
